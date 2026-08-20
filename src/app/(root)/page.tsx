c5f6730e4be8b55d50b609d76ac1aa6e42966ec6
kirito <kirito666codr@gmail.com>
2026-08-20 10:30:00 +0530

    feat: enhance animations in HeroSection and Cargo components; add Loader component for improved loading experience

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import Loader from '@/components/Loader';
 import AboutSection from './AboutSection';
 import FeaturedSection from './FeaturedSection';
 import FooterSection from './FooterSection';
@@ -5,11 +6,11 @@ import HeroSection from './HeroSection';
 
 export default function Home() {
   return (
-    <>
+    <Loader>
       <HeroSection />
       <AboutSection />
       <FeaturedSection />
       <FooterSection />
-    </>
+    </Loader>
   );
 }
