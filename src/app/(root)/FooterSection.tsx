4d0d41c709c255cb5ecc9475dbbe11fd5b08761c
kirito <kirito666codr@gmail.com>
2026-08-21 10:30:00 +0530

    feat: add GSAP animations to FooterSection and hide scrollbars in globals.css

--- src/app/(root)/FooterSection.tsx
+++ src/app/(root)/FooterSection.tsx
@@ -1,11 +1,25 @@
+'use client';
+import { gsap } from '@/libs/gsap';
+import { useGSAP } from '@gsap/react';
 import Image from 'next/image';
 
 export default function FooterSection() {
+  useGSAP(() => {
+    gsap.from('#footer-img', {
+      y: 400,
+      duration: 2,
+      scrollTrigger: {
+        trigger: '#footer',
+        start: 'top center',
+        end: 'bottom bottom',
+      },
+    });
+  });
   return (
-    <div className="bg-paper relative h-screen w-screen">
+    <div id="footer" className="bg-paper relative h-screen w-screen overflow-hidden">
       <div className="absolute top-0 left-0 h-full w-full">
         <Image src="/sky.png" fill alt="footer" className="object-cover" />
-        <div className="absolute bottom-0 left-0 h-full w-full">
+        <div id="footer-img" className="absolute bottom-0 left-0 h-full w-full">
           <Image
             src="/footer-cargo.png"
             fill
